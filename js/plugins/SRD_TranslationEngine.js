b341597943f83f67e90ad78355845f7ada81bca2
IkuTronHD <34961368+IkuTronHD@users.noreply.github.com>
2022-11-24 19:53:53 +0100

    Web

--- js/plugins/SRD_TranslationEngine.js
+++ js/plugins/SRD_TranslationEngine.js
@@ -449,7 +449,7 @@ Utils.isOptionValid = function(name) {
 
 $.params = PluginManager.parameters('SRD_TranslationEngine');
 
-$.isNewNWjs = process.versions['node-webkit'] >= "0.13.0";
+$.isNewNWjs = true; //$.isNewNWjs = process.versions['node-webkit'] >= "0.13.0";
 
 try {
 	$.languages = JSON.parse($.params['Languages']);
